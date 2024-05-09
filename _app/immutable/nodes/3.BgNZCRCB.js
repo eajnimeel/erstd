558f45297315058f29e769409db911a2fd0e53ae
Minjae Lee <minjae@bellins.net>
2024-05-09 15:03:58 +0900

    Updates

---
+++ _app/immutable/nodes/3.BgNZCRCB.js
@@ -0,0 +1,4 @@
+import{s as o,e as i,c,o as l,p,i as m,n as s,f as h}from"../chunks/scheduler.Bn2H72ui.js";import{S as f,i as d}from"../chunks/index.CeAxrHiT.js";function u(n){let e,a=`<h1 class="text-lg font-semibold">About page</h1>
+	ERD Space (<a href="https://www.erdspace.com/">https://www.erdspace.com/</a>) 의 ers 파일을 읽어 테이블 명세서를 만듭니다.<br/>
+	테이블 명세서는 기본값과 설명을 추가 하여 별도의 파일로 관리 할 수 있습니다.<br/>
+	모든 파일은 브라우저 상에서 처리 됩니다.(서버로 업로드하지 않습니다.)`;return{c(){e=i("main"),e.innerHTML=a,this.h()},l(t){e=c(t,"MAIN",{class:!0,"data-svelte-h":!0}),l(e)!=="svelte-1tiz3pe"&&(e.innerHTML=a),this.h()},h(){p(e,"class","p-2")},m(t,r){m(t,e,r)},p:s,i:s,o:s,d(t){t&&h(e)}}}class g extends f{constructor(e){super(),d(this,e,null,u,o,{})}}export{g as component};
