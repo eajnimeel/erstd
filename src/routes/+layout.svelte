d7e5373bbe91bb0ddb84ce94fd654d61fe5e7cd5
Minjae Lee <minjae@bellins.net>
2024-05-09 14:36:15 +0900

    path 설정 추가

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import {AppBar, AppLayout, NavItem, settings} from 'svelte-ux';
+    import { base } from '$app/paths';
 
     import {page} from '$app/stores';
     import '../app.postcss';
@@ -27,7 +28,7 @@
 <AppLayout>
     <nav slot="nav" class="nav h-full overflow-hidden">
         <NavItem
-                path="/"
+                path="{base}/"
                 text="Home"
                 icon="M10,20V14H14V20H19V12H22L12,3L2,12H5V20H10Z"
                 currentUrl={$page.url}
@@ -35,7 +36,7 @@
         />
 
         <NavItem
-                path="/about"
+                path="{base}/about"
                 text="About"
                 icon="M11,9H13V7H11M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M11,17H13V11H11V17Z"
                 currentUrl={$page.url}
