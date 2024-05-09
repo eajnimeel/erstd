195e2df4304ad99131d1895ef0402baf2911ea3d
Minjae Lee <minjae@bellins.net>
2024-05-09 14:43:37 +0900

    updates

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
     paths: {
-      base: '/erstd',
+      base: process.env.NODE_ENV === "production" ? '/erstd' : '',
       relative: false
     }
   },
